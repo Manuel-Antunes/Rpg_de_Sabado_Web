19215ca0b624064b97a4e13d0c2c017f460bcf0b
TetsuHikari <57446204+TetsuHikari@users.noreply.github.com>
2020-12-04 11:05:28 -0300

    feat : game screen

---
+++ src/pages/Game/index.tsx
@@ -0,0 +1,196 @@
+import React, { useState } from "react";
+import firebase from "firebase";
+import {
+  Container,
+  Fichas,
+  Jogadores,
+  Main,
+  MenuBar,
+  Categorias,
+  Chat,
+  Dices,
+} from "./styles";
+import Ficha from "../Ficha";
+import legolas from "../../resources/images/legolas.jpg";
+import d4 from "../../resources/images/d4.svg";
+import d6 from "../../resources/images/d6.svg";
+import d8 from "../../resources/images/d8.svg";
+import d10 from "../../resources/images/d10.svg";
+import d12 from "../../resources/images/d12.svg";
+import d20 from "../../resources/images/d20.svg";
+import d100 from "../../resources/images/d10.svg";
+import { Game as Mesa } from "../../types";
+import ded from "../../resources/images/dungeons.png";
+import { store } from "../../tsstore";
+
+const Game: React.FC = () => {
+  const [mesa, setMesa] = useState<Mesa>({
+    id: "1",
+    nome: "Dungeons&Dragons",
+    logo: ded,
+    players: 3,
+    maxPlayers: 10,
+  });
+  const [users, setUsers] = useState();
+  function getMinNome(nome: string): string {
+    const [a, b] = nome.split(" ");
+    return "" + a + " " + b;
+  }
+  const [categorias, setCategorias] = useState([
+    {
+      name: "a",
+      fichas: [
+        { owner: "a", name: "Legolas o Elfo", image: legolas },
+        { owner: "a", name: "a", image: legolas },
+        { owner: "a", name: "a", image: legolas },
+      ],
+    },
+    {
+      name: "a",
+      fichas: [
+        { owner: "a", name: "Legolas o Elfo", image: legolas },
+        { owner: "a", name: "a", image: legolas },
+        { owner: "a", name: "a", image: legolas },
+      ],
+    },
+    {
+      name: "a",
+      fichas: [
+        { owner: "a", name: "Legolas o Elfo", image: legolas },
+        { owner: "a", name: "a", image: legolas },
+        { owner: "a", name: "a", image: legolas },
+      ],
+    },
+  ]);
+  const [jogadores, setJogadores] = useState<
+    Array<firebase.User | null | undefined>
+  >([store.getState().user.data.user]);
+  const [guias, setGias] = useState([
+    { nome: "ficha 1" },
+    { nome: "ficha 2" },
+    { nome: "ficha 3" },
+  ]);
+  return (
+    <Container>
+      <Categorias>
+        <span>Fichas</span>
+        <ul>
+          {categorias.map((element) => (
+            <li>
+              <span>{element.name}</span>
+              <Fichas>
+                {element.fichas.map((ficha) => (
+                  <li>
+                    <img src={ficha.image} alt="" />
+                    <label htmlFor="">{ficha.name}</label>
+                  </li>
+                ))}
+              </Fichas>
+            </li>
+          ))}
+        </ul>
+      </Categorias>
+      <Main>
+        <ul>
+          {guias.map((guia) => (
+            <li>
+              <span>{guia.nome}</span>
+            </li>
+          ))}
+        </ul>
+        <div className="wrapper">
+          <Ficha />
+        </div>
+      </Main>
+      <Jogadores>
+        <span>Jogadores</span>
+        <ul>
+          {jogadores?.map((u) => (
+            <li>
+              <div className="profile">
+                <img src={u?.photoURL ? u?.photoURL : ""} alt="" />
+                <span>{getMinNome(u?.displayName ? u?.displayName : "")}</span>
+              </div>
+              <div className="campos"></div>
+            </li>
+          ))}
+        </ul>
+      </Jogadores>
+      <MenuBar>
+        <Dices>
+          <span>Rolar Dado</span>
+
+          <div className="dices">
+            <button>
+              <img src={d4} alt="" />
+              <span>D4</span>
+            </button>
+            <button>
+              <img src={d6} alt="" />
+              <span>D6</span>
+            </button>
+            <button>
+              <img src={d8} alt="" />
+              <span>D8</span>
+            </button>
+            <button>
+              <img src={d10} alt="" />
+              <span>D10</span>
+            </button>
+            <button>
+              <img src={d12} alt="" />
+              <span>D12</span>
+            </button>
+            <button>
+              <img src={d20} alt="" />
+              <span>D20</span>
+            </button>
+            <button>
+              <img src={d100} alt="" />
+              <span>D100</span>
+            </button>
+          </div>
+        </Dices>
+        <button>
+          <img src={mesa.logo} alt="" />
+        </button>
+        <Dices>
+          <span>Rolar Dado</span>
+
+          <div className="dices">
+            <button>
+              <img src={d4} alt="" />
+              <span>D4</span>
+            </button>
+            <button>
+              <img src={d6} alt="" />
+              <span>D6</span>
+            </button>
+            <button>
+              <img src={d8} alt="" />
+              <span>D8</span>
+            </button>
+            <button>
+              <img src={d10} alt="" />
+              <span>D10</span>
+            </button>
+            <button>
+              <img src={d12} alt="" />
+              <span>D12</span>
+            </button>
+            <button>
+              <img src={d20} alt="" />
+              <span>D20</span>
+            </button>
+            <button>
+              <img src={d100} alt="" />
+              <span>D100</span>
+            </button>
+          </div>
+        </Dices>
+      </MenuBar>
+    </Container>
+  );
+};
+
+export default Game;
---
+++ src/pages/Game/styles.tsx
@@ -0,0 +1,171 @@
+import { darken } from "polished";
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 2fr 1fr;
+  grid-template-rows: 1fr auto;
+`;
+export const Categorias = styled.div`
+  background-color: #191920;
+  border: #2f2f3b solid 5px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  ul {
+    width: 100%;
+    background-color: #141419;
+    li {
+      span {
+        margin-left: 20px;
+        color: #e65100;
+        font-size: 30px;
+      }
+    }
+  }
+  span {
+    font-size: 50px;
+    color: #e65100;
+    text-align: center;
+    font-family: "Kalam", cursive;
+  }
+`;
+export const Main = styled.div`
+  .wrapper {
+    height: 550px;
+
+    overflow: auto;
+    overflow-y: scroll;
+    &::-webkit-scrollbar {
+      width: 5px;
+      background: ${darken(0.2, "#e65100")};
+    }
+    &::-webkit-scrollbar-thumb {
+      background: #e65100;
+      -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.5);
+    }
+  }
+  ul {
+    display: flex;
+    border-bottom: 3px solid#2F2F3B;
+    li {
+      padding: 4px 0;
+      padding-right: 80px;
+      background-color: #141419;
+      border: 3px solid #2f2f3b;
+      border-bottom: none;
+      margin-left: 10px;
+
+      span {
+        font-size: 20px;
+        cursor: default;
+        color: #e65100;
+      }
+    }
+  }
+`;
+export const Jogadores = styled.div`
+  background-color: #191920;
+  border: #2f2f3b solid 5px;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  ul {
+    width: 100%;
+    li > * {
+      padding: 5px 10px;
+    }
+    li {
+      display: flex;
+      border: solid 1px #707070;
+      width: 100%;
+      .profile {
+        display: flex;
+        align-items: center;
+        font-size: 20px;
+        background-color: #141419;
+        color: #e65100;
+        img {
+          margin-right: 10px;
+          border-radius: 50%;
+          width: 50px;
+        }
+        span {
+          font-size: 20px;
+          text-shadow: -1px 0 #191920, 0 1px #191920, 1px 0 #191920,
+            0 -1px #191920;
+        }
+      }
+    }
+  }
+  span {
+    font-size: 50px;
+    color: #e65100;
+    text-align: center;
+    font-family: "Kalam", cursive;
+  }
+`;
+export const MenuBar = styled.div`
+  grid-column: 1/4;
+  display: flex;
+  button{
+    width: 20rem;
+    img{
+      width:100%;
+    }
+  }
+  justify-content: space-between;
+`;
+export const Fichas = styled.ul`
+  li > * {
+    margin-left: 30px;
+  }
+  li {
+    margin: 0 4px;
+    & + li {
+      margin-top: 3px;
+    }
+    border: 1px solid #707070;
+    background-color: #2f2f3b;
+    display: flex;
+    align-items: center;
+    img {
+      width: 30px;
+      border-radius: 50%;
+    }
+    label {
+      color: #e65100;
+      font-size: 20px;
+      text-shadow: -1px 0 #191920, 0 1px #191920, 1px 0 #191920, 0 -1px #191920;
+    }
+  }
+`;
+export const Dices = styled.div`
+  display: flex;
+  flex-direction: column;
+  div {
+    display: flex;
+    button {
+      background:none;
+      border:none;
+      width:5rem;
+      img {
+        width:100%;
+      }
+      span{
+        font-size: 25px;
+      }
+      flex-direction: column;
+      display: flex;
+      justify-content:center;
+      align-items:center;
+    }
+  }
+  span {
+    font-size: 50px;
+    color: #e65100;
+    text-align: center;
+    font-family: "Kalam", cursive;
+  }
+`;
+export const Chat = styled.div``;
