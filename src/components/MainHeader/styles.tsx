69e7de2d9175c251b136e406e2a81f04dfb75cb7
TetsuHikari <57446204+TetsuHikari@users.noreply.github.com>
2020-12-04 11:00:25 -0300

    feat : MainHeader

---
+++ src/components/MainHeader/styles.tsx
@@ -0,0 +1,18 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  position: relative;
+  padding: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  h1 {
+    color: #e65100;
+    font-size: 7rem;
+  }
+  img {
+    position: absolute;
+    height: 100%;
+    z-index: -1;
+  }
+`;
