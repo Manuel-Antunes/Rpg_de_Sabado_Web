04ec06f92c670ddbfbc731c36622afe83ea720b0
TetsuHikari <57446204+TetsuHikari@users.noreply.github.com>
2020-12-04 10:56:34 -0300

    feat : GoogleButton and login

--- src/components/GoogleButton/index.tsx
+++ src/components/GoogleButton/index.tsx
@@ -1,10 +1,15 @@
 import React from "react";
-
+import { useDispatch } from "react-redux";
 import google from "../../resources/images/XD/google.svg";
 import firebase from "../../services/firebase";
+import { store } from "../../tsstore";
+import { AuthTypes } from "../../tsstore/ducks/auth/types";
+import { User } from "../../tsstore/ducks/user/types";
+import history from "../../services/history";
 // import { Container } from './styles';
 
 const GoogleButton: React.FC = () => {
+  const dispatch = useDispatch();
   return (
     <button
       style={{ background: "none", border: "none" }}
@@ -19,6 +24,13 @@ const GoogleButton: React.FC = () => {
             e.user?.getIdToken().then((a) => {
               console.log(a);
             });
+            const payload: User = { user: e.user };
+            dispatch({
+              type: AuthTypes.SIGN_IN_SUCCESS,
+              payload,
+            });
+            history.push("/");
+            console.log(store.getState());
           })
           .catch((err) => {
             console.log(err);
